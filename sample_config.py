8e70f96321d5d3333b3ae1687e7b321c85cd6783
MafiaFight <80632083+MafiaFight@users.noreply.github.com>
2021-03-14 22:16:11 +0530

    Add files via upload

--- sample_config.py
+++ sample_config.py
@@ -4,12 +4,12 @@ class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    DB_URI = os.environ.get("DATABASE_URL", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", False)
+    DB_URI = os.environ.get("DATABASE_URL", False)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", False)
     LOGGER = True
-    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", False)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", False)
     # Here for later purposes
     SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "924138714").split())
     WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
@@ -17,25 +17,25 @@ class Var(object):
     DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "").split())
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
     SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None)) 
-    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
-    DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
-    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", None)
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", False)) 
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", False)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", False)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", False)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", False)
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", False)
+    DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", False)
+    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", False)
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", False)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", False)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
-    if AUTH_TOKEN_DATA != None:
+    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", False)
+    if AUTH_TOKEN_DATA != False:
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
         t_file = open(TEMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
         t_file.write(AUTH_TOKEN_DATA)
         t_file.close()
-    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
-    if PRIVATE_GROUP_ID != None:
+    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", False)
+    if PRIVATE_GROUP_ID != False:
         try:
             PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
         except ValueError:
@@ -43,4 +43,4 @@ class Var(object):
 
 
 class Development(Var):
-    LOGGER = True
+    LOGGER = False
